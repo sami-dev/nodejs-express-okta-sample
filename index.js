2698ac3d5801bb7004cb9bfb0840c2e4e0496ff1
Sami Abdul <sami.abdul@gmail.com>
2021-06-06 02:49:09 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const session = require("express-session");
 const { ExpressOIDC } = require("@okta/oidc-middleware");
 
 const app = express();
+const path = require("path");
 const oidc = new ExpressOIDC({
-  issuer: "https://example.oktapreview.com/oauth2/ausfby7lsqSlV4VZY0h7",
-  client_id: "",
-  client_secret: "",
+  issuer: "https://dev-217355.oktapreview.com/oauth2/default",
+  client_id: "0oazmcgd4iRiO4h4T0h7",
+  client_secret: "DxNfeKFYd574iRDK0_K8e7N42R8HXVc5XLezZ189",
   appBaseUrl: "http://localhost:3000",
   scope: "openid profile",
 });
@@ -28,7 +29,8 @@ app.get("/", (req, res) => {
       </form>
     `);
   } else {
-    res.send('Please <a href="/login">login</a>');
+    //res.send('Please <a href="/login">login</a>');
+    res.sendFile(path.join(__dirname, "/index.html"));
   }
 });
 
